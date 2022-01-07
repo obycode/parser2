b1c199350de68512b2da3e517b3cd9686bb7a501
Brice Dobry <brice@hiro.so>
2022-01-07 17:47:58 -0500

    Basic functionality

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -13,18 +13,21 @@ pub struct Lexer<'a> {
 
 impl<'a> Lexer<'a> {
     pub fn new(input: &'a str) -> Self {
-        Self {
+        let mut s = Self {
             input: input.chars(),
-            next: '\0',
+            next: 0 as char,
             offset: 0,
             line: 1,
-            column: 1,
-        }
+            column: 0,
+        };
+        s.read_char();
+        s
     }
 
     pub fn read_char(&mut self) {
         if self.next == '\n' {
             self.line = self.line + 1;
+            self.column = 0;
         }
 
         match self.input.next() {
@@ -35,16 +38,14 @@ impl<'a> Lexer<'a> {
         self.column = self.column + 1;
     }
 
-    pub fn skip_whitespace(&mut self) -> bool {
-        let mut found = false;
+    pub fn skip_whitespace(&mut self) {
         while self.next != '\0' {
             match self.next {
-                ' ' | '\t' | '\r' | '\n' => found = true,
+                ' ' | '\t' | '\r' | '\n' => (),
                 _ => break,
             }
             self.read_char();
         }
-        found
     }
 
     pub fn read_line(&mut self) -> Vec<char> {
@@ -64,11 +65,13 @@ impl<'a> Lexer<'a> {
         line
     }
 
-    pub fn read_identifier(&mut self, first: char) -> Vec<char> {
-        let mut ident = vec![first];
+    pub fn read_identifier(&mut self, first: Option<char>) -> Vec<char> {
+        let mut ident = vec![];
+        if let Some(first) = first {
+            ident.push(first);
+        }
 
         loop {
-            self.read_char();
             match self.next {
                 'a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-' | '!' | '?' => ident.push(self.next),
                 _ => return ident,
@@ -79,7 +82,7 @@ impl<'a> Lexer<'a> {
 
     pub fn read_unsigned(&mut self) -> u128 {
         let mut num: u128 = 0;
-        while self.next.is_numeric() {
+        while self.next.is_ascii_digit() {
             let digit = self.next as u32 - '0' as u32;
             num = num * 10 + digit as u128;
             self.read_char();
@@ -89,7 +92,7 @@ impl<'a> Lexer<'a> {
 
     pub fn read_integer(&mut self) -> i128 {
         let mut num: i128 = 0;
-        while self.next.is_numeric() {
+        while self.next.is_ascii_digit() {
             let digit = self.next as u32 - '0' as u32;
             num = num * 10 + digit as i128;
             self.read_char();
@@ -98,7 +101,6 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn read_token(&mut self) -> PlacedToken {
-        self.skip_whitespace();
         let start_line = self.line as u32;
         let start_column = self.column as u32;
         let mut advance = true;
@@ -145,17 +147,24 @@ impl<'a> Lexer<'a> {
                 }
             }
             'u' => {
+                advance = false;
                 self.read_char();
-                if self.next.is_numeric() {
+                if self.next.is_ascii_digit() {
                     Token::Uint(self.read_unsigned())
                 } else {
-                    Token::Ident(self.read_identifier('u'))
+                    Token::Ident(self.read_identifier(Some('u')))
                 }
             }
+            ' ' | '\t' | '\r' | '\n' => {
+                self.skip_whitespace();
+                advance = false;
+                Token::Whitespace
+            }
             _ => {
+                advance = false;
                 if self.next.is_ascii_alphabetic() {
-                    Token::Ident(self.read_identifier(self.next))
-                } else if self.next.is_numeric() {
+                    Token::Ident(self.read_identifier(None))
+                } else if self.next.is_ascii_digit() {
                     Token::Int(self.read_integer())
                 } else {
                     Token::Invalid
@@ -172,7 +181,7 @@ impl<'a> Lexer<'a> {
                 start_line,
                 start_column,
                 end_line: self.line as u32,
-                end_column: self.column as u32,
+                end_column: (self.column - 1) as u32,
             },
             token,
         }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 mod lexer;
 use crate::lexer::token::Token;
+use std::io::{self, BufRead, Read};
 
 fn main() {
-    let mut lex = lexer::Lexer::new("(define-data-var current uint u0)");
+    let mut stdin = io::stdin();
+    let mut input = String::new();
+    if let Err(e) = stdin.read_to_string(&mut input) {
+        println!("Error reading from stdin: {}", e);
+        return;
+    }
+    let mut lex = lexer::Lexer::new(input.as_str());
     loop {
         let token = lex.read_token();
         println!(
